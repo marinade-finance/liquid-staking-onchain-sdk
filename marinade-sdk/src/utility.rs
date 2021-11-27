856ad2fd970a505884d6dec6390ba0dd7f7d7038
Flow_Biboux <florianesm@gmail.com>
2021-11-27 09:33:34 +0100

    dependency update

---
+++ marinade-sdk/src/utility.rs
@@ -0,0 +1,14 @@
+///constant
+pub mod constant;
+///error
+pub mod error;
+///fees
+pub mod fees;
+///instructions
+pub mod instructions;
+///processor
+pub mod processor;
+///states
+pub mod states;
+///utils
+pub mod utils;
